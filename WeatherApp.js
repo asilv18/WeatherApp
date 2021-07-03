adf0c9c080a85fc953f6c0acd7f81fbb1b648854
Aimee Davidson <adavidson1802@hotmail.co.uk>
2021-07-03 16:04:09 +0100

    Update WeatherApp.js

--- WeatherApp.js
+++ WeatherApp.js
@@ -56,7 +56,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey ="267c42e32a005ebd3d86d6022f9f9baf";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}& units=metric`;
-console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeatherCondition(response) {
@@ -90,9 +90,10 @@ function getPosition(event) {
 let currentLocation = document.querySelector("#current");
 currentLocation.addEventListener("click", getPosition);
 searchCity("London");
-displayForecast();
 
- function displayForecast()  {
+
+ function displayForecast(response)  {
+   console.log(response.data.daily);
    let forecastElement = document.querySelector ("#forecast");
    let forecastHTML = `<div class="row predictions">`;
    let days = ["Sun",  "Mon",  "Tues",  "Weds",  "Thurs",  "Fri"];
