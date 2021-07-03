ed8d372b03caeffdcae170cf447899e446a750fa
Aimee Davidson <adavidson1802@hotmail.co.uk>
2021-07-03 17:01:53 +0100

    Update WeatherApp.js

--- WeatherApp.js
+++ WeatherApp.js
@@ -64,7 +64,7 @@ function searchCity(city) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey ="267c42e32a005ebd3d86d6022f9f9baf";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}& units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 
